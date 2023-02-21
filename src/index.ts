719b4b9593b266ab795ec658679802b5fcdd837d
Robin <66969441+CactusHamster@users.noreply.github.com>
2023-02-21 08:54:33 -0700

    (initial commit) awful mess... but it works :3

---
+++ src/index.ts
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+import { createConnection, NetConnectOpts, Socket, createServer } from "node:net";
+const server = createServer()
+server.on("error", (e) => console.error("Server error: ", e))
+type Method = "POST" | "CONNECT" | "PATCH" | "DELETE" | "GET" | string;
+const listener = server.listen(8080, "127.0.0.1")
+listener.on("listening", () => {
+    let address = listener.address();
+    if (!address) address = "NULL";
+    if (!(typeof address == "string")) address = `${address.address}:${address.port}`
+    console.log(`Server listening on ${address}.`);
+});
+
+async function createConnectionAsync (options: NetConnectOpts): Promise<Socket> {
+    return new Promise(resolve => {
+        let socket: Socket = createConnection(options, () => resolve( socket ))
+        socket.on("error", (e) => console.error("Outgoing socket error: ", e))
+    })
+}
+
+function parseHeaders (packet: string | Buffer): { method: Method; uri: string; version: string; headers: { [key: string]: string; }; } {
+    if (typeof packet !== "string") packet = packet.toString("utf-8");
+    let endIndex = packet.indexOf("\r\n\r\n")
+    if (endIndex === -1) endIndex = packet.length;
+    const lines = packet.toString().slice(0, 65535).slice(0, packet.indexOf("\r\n\r\n")).split("\r\n");
+    let headers: { [key: string]: string } = {};
+    lines.slice(1).forEach(line => {
+        let index = line.indexOf(": ");
+        headers[line.slice(0, index).toLowerCase()] = line.slice(index + 2);
+    })
+    let splitLine = lines[0].split(" ")
+    let method = splitLine[0]
+    let uri = splitLine[1]
+    let version = splitLine[2]
+//    for (let name in headers) console.log(`${name}: ${headers[name]}`)
+    return {
+        method,
+        uri,
+        version,
+        headers
+    }
+}
+
+function parseURI (uri: string): { port: number, host: string } {
+    let ci = uri.indexOf(":")
+    let host = uri.slice(0, ci)
+    let port: number | null = parseInt(uri.slice(ci + 1), 10)
+    return { host, port }
+}
+
+function proxySocket (incomingSocket: Socket, outgoingSocket: Socket) {
+    incomingSocket.pipe(outgoingSocket)
+    outgoingSocket.pipe(incomingSocket)
+}
+
+async function onFirstPacket (socket: Socket, packet: Buffer) {
+    console.log(packet.toString() + "*".repeat(50) + "\n".repeat(5))
+    let host: string, port: number, keepAlive: boolean;
+    let { method, uri, headers } = parseHeaders(packet)
+    let isTLS = method === "CONNECT"
+    if (isTLS) {
+        let t = parseURI(uri);
+        port = isNaN(t.port) ? 443 : t.port;
+        host = t.host
+    } else {
+        if (!headers["host"]) return socket.end("HTTP/1.1 500 BAD REQUEST\r\n\n")
+        let t = parseURI(headers["host"]);
+        port = isNaN(t.port) ? 80 : t.port;
+        host = t.host
+    }
+    keepAlive = headers["proxy-connection"] === "keep-alive";
+    let outgoingSocket = await createConnectionAsync({ host, port, keepAlive });
+    if (isTLS) socket.write('HTTP/1.1 200 OK\r\n\n');
+    else outgoingSocket.write(packet);
+    proxySocket(socket, outgoingSocket)
+}
+
+server.on("connection", (socket) => socket.once("data", (packet: Buffer) => onFirstPacket(socket, packet)))
---
+++ src/test-website.ts
@@ -0,0 +1,75 @@
+import { createServer as createHTTPServer, IncomingMessage, ServerResponse, Server as NodeHTTPServer } from "node:http";
+import { createServer as createHTTPSServer } from "node:https";
+import { readFileSync } from "node:fs";
+import { createHash } from "node:crypto";
+
+type SimpleServer = NodeHTTPServer<typeof IncomingMessage, typeof ServerResponse>
+type Response = ServerResponse<IncomingMessage> & { req: IncomingMessage }
+
+class Server {
+    http: SimpleServer
+    https: SimpleServer
+    constructor (
+        httpHandler: (req: IncomingMessage, res: Response) => any,
+        //wsHandler
+    ) {
+        this.http = createHTTPServer(httpHandler)
+        this.https = createHTTPSServer({
+            key: readFileSync("./localhost.key"),
+            cert: readFileSync("./localhost.crt")
+        }, httpHandler);
+        [this.http, this.https].forEach((server) => {
+            server.on("upgrade", (req, socket) => {
+                socket.on("data", (d) => console.log(Array.from(d).map((c) => "0x" + (c as number).toString(16))))
+                console.log("a client is requesting we use websockets")
+                if (req.headers["upgrade"] !== "websocket" || !req.headers["sec-websocket-key"]) {
+                    console.log("received a bad request")
+                    return socket.end("HTTP/1.1 400 Bad Request");
+                }
+                console.log("calculating their response key rq...")
+                const acceptKey = req.headers['sec-websocket-key'];
+                const hash = createHash('sha1').update(acceptKey + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', 'binary').digest('base64');
+                console.log(`Client key: ${acceptKey}\nOur key: ${hash}`)
+                console.log("response key calculated, returning headers now!")
+                const responseHeaders = [
+                    'HTTP/1.1 101 Switching Protocols',
+                    'Upgrade: websocket',
+                    'Connection: Upgrade',
+                    `Sec-WebSocket-Accept: ${hash}`
+                ]; 
+                socket.write(responseHeaders.concat('\r\n').join('\r\n'));
+            })
+        })
+        this.http.listen(80)
+        this.https.listen(443)
+    }
+}
+
+function httpHandler (req: IncomingMessage, res: Response) {
+    if (req.headers["upgrade"]) console.log("the client sent an upgrade header: " + req.headers["upgrade"])
+    res.writeHead(200, "OK")
+    res.write(`<!doctype html>
+<html>
+    <head>
+        <title>hi</title>
+    </head>
+    <body>
+        <p>success</p>
+        <p id="ws-success">websocket connecting...</p>
+    </body>
+    <script>
+        let ws = new WebSocket("ws://127.0.0.1/");
+        ws.addEventListener("open", () => {
+            console.log("websocket opened")
+            document.getElementById("ws-success").innerText = "websocket connected"
+        })
+        ws.addEventListener("error", (event) => {
+            console.log('WebSocket error: ', event)
+            document.getElementById("ws-success").innerText = "websocket error"
+        })
+    </script>
+</html>`)
+    res.end()
+}
+
+let server = new Server(httpHandler)
